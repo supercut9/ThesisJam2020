2ffc31db36c8edcf55d5975958e7c28d387a3ae3
supercut9(yamiao liu) <ymliu97@gmail.com>
2020-08-30 17:16:03 -0400

    add start scene
    all audio edited

---
+++ Water Ghost Game/Assets/Scripts/LoadLevel1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel1 : MonoBehaviour
+{
+
+
+    public AudioClip Drowning;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(LoadDrowning("MainScreen"));
+    }
+
+    IEnumerator LoadDrowning(string levell)
+    {
+        yield return new WaitForSeconds(20.0f); // wait time
+        GetComponent<AudioSource>().PlayOneShot(drowning1);
+
+        yield return new WaitForSeconds(10.0f);
+
+        Application.LoadLevel(levell);
+    }
+
+}
