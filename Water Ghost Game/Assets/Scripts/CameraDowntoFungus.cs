51875a66de9d55d0c0167be0ac319e7982bdc26d
mengqigao <mengqigao7@gmail.com>
2020-09-01 10:41:49 +0800

    test branch

---
+++ Water Ghost Game/Assets/Scripts/CameraDowntoFungus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class CameraDowntoFungus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Flowchart.BroadcastFungusMessage("messageAboutCameraDown");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      //Debug.Log("stop changing");
+      //if (Input.anyKeyDown) {
+      //Flowchart.BroadcastFungusMessage("time to start");
+      //}
+
+
+    }
+}
