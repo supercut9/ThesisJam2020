107b1e2bdfff89b943ca11fb501444ebd4e798ec
mengqigao <mengqigao7@gmail.com>
2020-09-01 20:31:05 +0800

    Just a Backup
    和上一次 commit 内容相同
    我 github update from master 之后开始疯狂报错

---
+++ Water Ghost Game/Assets/Scripts/CheckBeta.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class CheckBeta : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SpriteRenderer _rend;
+    public GameObject _RefCameraUp;
+
+    void Start()
+    {
+        _rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_rend.color.a == 0f)
+        {
+            //this.SetActive(false);
+            Debug.Log("Received");
+            //Flowchart.BroadcastFungusMessage("Camera Up 1");
+            _RefCameraUp.SetActive(true);
+        }
+    }
+}
